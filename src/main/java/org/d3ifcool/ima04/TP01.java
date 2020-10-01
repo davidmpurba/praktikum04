1c9b8ce6a94972389fb0c294df68ffd9e6f3dbf8
David <71245148+davidmpurba@users.noreply.github.com>
2020-10-01 07:13:45 +0700

    Update TP01.java

--- src/main/java/org/d3ifcool/ima04/TP01.java
+++ src/main/java/org/d3ifcool/ima04/TP01.java
@@ -4,5 +4,14 @@ public class TP01 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
+		Scanner in = new Scanner(System.in);
+
+
+      int n = in.nextInt();
+
+    if(n == 6) 
+	System.out.println("enam");
+    else 
+	System.out.println("bukan enam");
 	}
 }
