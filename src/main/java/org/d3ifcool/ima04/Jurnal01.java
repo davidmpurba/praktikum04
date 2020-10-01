502d81780bdb40521b471c0649b542d1b1a7b386
David <71245148+davidmpurba@users.noreply.github.com>
2020-10-01 07:24:11 +0700

    Update Jurnal01.java

--- src/main/java/org/d3ifcool/ima04/Jurnal01.java
+++ src/main/java/org/d3ifcool/ima04/Jurnal01.java
@@ -4,5 +4,15 @@ public class Jurnal01 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
+		Scanner in = new Scanner(System.in);
+		int bilangan;
+		
+		
+		bilangan = in.nextInt();
+		
+		if(bilangan%6 == 0)
+			System.out.println("kelipatan enam");
+		else
+			System.out.println("bukan kelipatan enam");
 	}
 }
